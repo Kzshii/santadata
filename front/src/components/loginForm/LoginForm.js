30b8e68f0f00719d80d8ac50b0a508700ef9922f
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-01-16 14:14:51 -0200

    Test(Login Page)

--- front/src/components/loginForm/LoginForm.js
+++ front/src/components/loginForm/LoginForm.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import './LoginForm.css';
+import axios from 'axios';
 
 class LoginForm extends Component {
 
   constructor(props) {
     super(props);
 
-    this.handleSubmit = this.handleSubmit.bind();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeEmail  = this.handleChangeEmail.bind(this);
+    this.handleChangePassword  = this.handleChangePassword.bind(this);
+
+    this.state = {
+      user: '',
+      pass: '',
+    };
+  }
+
+  handleChangeEmail(event) {
+    this.setState({user: event.target.value});
+  }
+
+  handleChangePassword(event) {
+    this.setState({pass: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('login');
+    /* alert('login'); */
+
+    const server = 'http://restfull.hol.es/les/user/login/am9yZGFu/MTIz/';
+
+    axios.get(server)
+    .then(function(response){
+      console.log(response.data);
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+
     event.preventDefault();
   }
 
@@ -19,8 +46,8 @@ class LoginForm extends Component {
     return(
       <div className="LoginForm">
         <form onSubmit={this.handleSubmit} >
-          <input type="email" placeholder="email" /> <br/>
-          <input type="password" placeholder="senha" /> <br/>
+          <input type="email" placeholder="email" value={this.state.user} onChange={this.handleChangeEmail} /> <br/>
+          <input type="password" placeholder="senha" value={this.state.pass} onChange={this.handleChangePassword} /> <br/>
           <input type="checkbox" id="rememberUserCheck" />
           <label htmlFor="rememberUserCheck">Lembre de mim</label> <br/>
           <input type="submit" value="Entrar" />
