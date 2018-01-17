9518d4d4e60b935ce63544f51596583be4aadbbb
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-01-17 16:45:25 -0200

    Feat(Generic decoding data)

--- rest/controller/generics/user.js
+++ rest/controller/generics/user.js
@@ -24,38 +24,42 @@ var User = Object.create(Base);
 User.new_user = function(req, res){
 
 	var var_req = req.body;
+	var_req = User.decode_data(var_req)
 
 	//Check authentication
-	if(!User.check_requisition(req)){
-		res.status(500).send(User.error_message("Bad request"));
+	if(!User.check_requisition(req) || Object.keys(var_req).length == 0){
+		res.status(404).send(User.error_message("Invalid request"));
 		return
 	}
 
-	var sql = "SELECT new_user(?,?,?,?,?,?);";
-	sql = Mysql.format(sql, [var_req.name, 
-			var_req.email,
-			var_req.login,
-			var_req.pass,
-			"-void-hash-",
-			parseInt(var_req.type_user)]);
+	try{
+		sql = Mysql.format(sql, [var_req.name, 
+				var_req.email,
+				var_req.login,
+				var_req.pass,
+				"-void-hash-",
+				parseInt(var_req.type_user)]);
 
-	Mysql.query(sql, function (err, results) {
-		if(err) { res.send(500,"Database error"); return; }
-		result = results[0]
-		keys = Object.keys(result)
-		result = keys.map(function(k){return result[k]})
-		var response = {
-			success: 1,
-			data:{iduser:result[0]}
-		}
-		
-		if(result.length == 0)
-			response.success = 0
+		Mysql.query(sql, function (err, results) {
+			if(err) { res.send(500,"Database error"); return; }
+			result = results[0]
+			keys = Object.keys(result)
+			result = keys.map(function(k){return result[k]})
+			var response = {
+				success: 1,
+				data:{iduser:result[0]}
+			}
+			
+			if(result.length == 0)
+				response.success = 0
 
-		console.log("# Inserted user: "+response.data.iduser)
-		res.send(JSON.stringify(response));		
-	});
-
+			console.log("# Inserted user: "+response.data.iduser)
+			res.send(JSON.stringify(response));		
+		});
+	}
+	catch(e){
+		res.status(500).send(User.error_message("Bad request"));
+	}
 }
 
 
