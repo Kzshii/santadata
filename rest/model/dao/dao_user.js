dd4e6322557895aae0655f9ed0a2bfd088a6a931
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-01-18 13:06:26 -0200

    Feat(Encapsulating callback)

--- rest/model/dao/dao_user.js
+++ rest/model/dao/dao_user.js
@@ -9,31 +9,41 @@
 var Dao = require( './dao.js' );
 var Dao_user = Object.create(Dao);
 
-Dao_user.new_user = function(data, callback, param){
+Dao_user.callback_new_user = function(param,ret){
+	var callback = param.callback
+	var par = param.param
 
-		var query = "SELECT new_user(?,?,?,?,?,?);"
-		var ret = this.mysql_query(query, data, callback, param);
-
-		//Checking error
-		if(ret.success == 0){
-			callback(param,ret)
-			return
-		}
-
-		//Processing data result
-		result = ret.data[0]
-		keys = Object.keys(result)
-		result = keys.map(function(k){return result[k]})
-		
-		//Setting return data
-		ret.data = {iduser:result[0]}
-		if(result.length == 0)
-			ret.success = 0
-
-		//Returning data
-		console.log("# Inserted user: "+ret.data.iduser)
-		//res.send(JSON.stringify(response));		
+	//Checking error
+	if(ret.success == 0){
 		callback(param,ret)
+		return
+	}
+
+	//Processing data result
+	result = ret.data[0]
+	keys = Object.keys(result)
+	result = keys.map(function(k){return result[k]})
+	
+	//Setting return data
+	ret.data = {iduser:result[0]}
+	if(result.length == 0)
+		ret.success = 0
+
+	//Returning data
+	console.log("# Inserted user: "+ret.data.iduser)
+	//res.send(JSON.stringify(response));		
+	callback(par,ret)	
 }
+
+Dao_user.new_user = function(param, data, callback){
+	var par = {
+		param = param,
+		callback = callback
+	}
+
+	var query = "SELECT new_user(?,?,?,?,?,?);"
+	this.mysql_query(query, data, Dao_user.callback_new_user, par);
+}
+
 
 module.exports = Dao_user
