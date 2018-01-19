7b7111c977c6ff376ce5196113362979961d9a60
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-01-19 00:52:21 -0200

    Fix(route)

--- rest/routes/generics/exams.js
+++ rest/routes/generics/exams.js
@@ -21,7 +21,7 @@ module.exports = function (app) {
 	Exams.add_route("/gen/search/exam/physical/", c_physical_exam.search, Exams.sufix.end_route, Exams.type.post)
 	Exams.add_route("/gen/get/exam/physical/", c_physical_exam.get, Exams.sufix.id_route, Exams.type.post)
 	//Exams.add_route("/gen/all/patient/", c_physical_exam.all, Exams.sufix.end_route, Exams.type.post)
-	Exams.add_route("/gen/update//exam/physical/", c_physical_exam.update, Exams.sufix.id_route, Exams.type.post)
+	Exams.add_route("/gen/update/exam/physical/", c_physical_exam.update, Exams.sufix.id_route, Exams.type.post)
 	
 	//Other exam
 
