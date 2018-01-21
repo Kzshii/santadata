e2aa1ffd696f451a78d78f90a0829573ec8fd651
Gary M. Silva <garymsilva@outlook.com>
2018-01-21 12:30:36 -0200

    Feat(Patient profile): Create page division

---
+++ front/src/components/infoCard/InfoCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './infoCard.css';
+
+class InfoCard extends Component {
+  render() {
+    return(
+      <p>card</p>
+    );
+  }
+}
+
+export default InfoCard;
--- front/src/components/patientProfile/PatientProfile.js
+++ front/src/components/patientProfile/PatientProfile.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './PatientProfile.css';
+import InfoCard from './../infoCard/InfoCard';
 
 class PatientProfile extends Component {
 
   render() {
     return(
-      <p>Perfil</p>
+      <div className="PatientProfile">
+        <div className="patientData">
+          <InfoCard data="" />
+        </div>
+        <div className="allergies"></div>
+        <div className="currentState"></div>
+        <div className="ICFER"></div>
+        <div className="comorbidities"></div>
+        <div className="timeLine"></div>
+      </div>
     );
   }
 }
