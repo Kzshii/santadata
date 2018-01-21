846b895d9c02db7aba01870ea5ff38f3518f5b52
JoséGuilherme <guislima47@gmail.com>
2018-01-21 16:17:54 -0200

    Feat(Patient profile): Fixed SearchPatient function

--- front/src/components/patientProfile/PatientProfile.js
+++ front/src/components/patientProfile/PatientProfile.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
 import './PatientProfile.css';
 import InfoCard from './../infoCard/InfoCard';
+import Base64 from './../../lib/base64';
+import axios from 'axios';
+
 
 class PatientProfile extends Component {
+
+  constructor(props){
+
+    super(props)
+
+      
+    this.state= {
+      userData: null,
+
+    }
+
+    this.searchPatient(1)
+    
+
+  }
+
+  searchPatient(id) {
+    var patientId= 4 /* this.props.id */
+    
+    axios.defaults.baseURL = 'https://31.220.54.251:8443/';
+    
+     axios.post(
+      'gen/get/patient/'+patientId+'/1/MTY2Mjg5N2IzY2IyODBjOTA0NjE4M2QwMzg3ZGYzYzk=/', 
+      'data='+Base64.encode(
+        {}
+      )
+    )
+    .then(
+      (response)=>{
+        this.setState({
+          userData: response.data.data[0]
+        })
+
+        console.log(this.state)
+      }
+    ).catch(); 
+    
+    
+    
+    /* test block */
+  /*   return({
+      data: [
+        {name: "Arthur", id: "55"},{name: "Arthur Cristo", id: "58"}
+      ]
+    }); */
+  }
+
+
+
+
+
+
+
+
 
   render() {
     return(
