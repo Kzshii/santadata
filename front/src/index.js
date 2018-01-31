7cbd878728454f244fad8b8b86661419194bb2ff
Gary M. Silva <garymsilva@outlook.com>
2018-01-31 20:21:32 -0200

    Fix(package.json): Remove unused dependencies

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/app/App';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
