7ea315d89ae1c57fd40f99970e94f2fdb78b9cc5
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-02-02 00:18:37 -0200

    Test

--- rest/controller/autenticator.js
+++ rest/controller/autenticator.js
@@ -116,6 +116,8 @@ var Autenticator = {
 		//Calling Dao Function
 		Dao_user.login(res,data, function(res,result){
 			console.log("teste")
+			console.log(result.data.length)
+			console.log(result.data)
 			if(result.success == 0){
 				res.status(result.error).send(error_message(result.message));
 			}
