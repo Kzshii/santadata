802c7eab93d18bd198b9982d23312b44f1c531c2
Gary M. Silva <garymsilva@outlook.com>
2018-02-02 00:42:47 -0200

    Feat(Lib Axios): Post method refactored

--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -2,27 +2,56 @@ import axios from 'axios';
 import Config from './../config.json';
 import Base64 from './base64';
 
-axios.defaults.baseURL = Config.rest.baseURL;
+Post.urlData = [];
+Post.data = {};
 
-export function Post(route, data, urlData) {
+Post.command = (data) => {
+  console.log("POST - DEFAULT COMMAND",data);
+  return(data);
+}
 
-  let urlComplete = "";
+export function Post(route, urlData, data) {
 
-  for(let i = 0; i < urlData.lenght ; i++) {
-    urlComplete = urlComplete + urlData[i] + "/";
+  if(route === undefined) {
+    console.log("Route missing");
+    return null;
   }
 
+  if(Post.urlData === [] && urlData !== undefined && Array.isArray(urlData)) {
+    Post.urlData = urlData;
+  }
+
+  if(Post.data === {} && data !== undefined) {
+    Post.data = data;
+  }
+
+  let urlComplement = "";
+
+  for(let i = 0; i < Post.urlData.lenght ; i++) {
+    urlComplement = urlComplement+Post.urlData[i]+"/";
+  }
+
+  let URL = Config.rest.routes[route]+urlComplement;
+
+  axios.defaults.baseURL = Config.rest.baseURL;
   axios.post(
-    Config.rest.routes[route]+urlComplete,
-    "data="+Base64.encode(data)
+    URL,
+    "data="+Base64.encode(Post.data)
   )
   .then(
     function(response) {
-      return(response.data.data);
+      if(response.request.status == 200) {
+        console.log("LIB AXIOS - THEN OK:",response);
+        Post.command(response.data.data);
+      } else {
+        console.log("LIB AXIOS - THEN FAIL:",response);
+        return(null);
+      }
     }
   )
   .catch(
     function(error) {
+      console.log("LIB AXIOS - CATCH:",error);
       return(error);
     }
   );
