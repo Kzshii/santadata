adb83e245635b0eb309c5dfd17f48380bf769a41
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-02-19 16:49:26 -0300

    Update(Prepare): Medicines, Interventions

--- rest/model/interventions.js
+++ rest/model/interventions.js
@@ -13,18 +13,30 @@ Interventions.types = {
 	void: {id: -1, label: "Vazio"},
 
 	// Tipos de intervenções
-	angio:
-	[
-		{id: 0,label: "Cirurgia de Revascularização do Miocárdio (CRM) prévia"},
-		{id: 1,label: "Intervenção Coronária Percutanea (ICP) prévia"}
-	],
+	 angio:
+    {
+        type: "select",
+        title: "Angio",
+        required: "true",
+        options: 
+        [
+            {id: 0,label: "Cirurgia de Revascularização do Miocárdio (CRM) prévia"},
+            {id: 1,label: "Intervenção Coronária Percutanea (ICP) prévia"},
+        ]
+    },
 
-	implantes: 
-	[
-		{id: 0,label: "Marcapasso Definitivo (MPD)"},
-		{id: 1,label: "Cardiodesfibrilador Implantável (CDI)"},
-		{id: 2,label: "Tratamento por Ressincronização Cardíaca (TRC)"}
-	]
+    implantes: 
+    {
+        type: "select",
+        title: "Implantes",
+        required: "true",
+        options: 
+        [
+            {id: 0,label: "Marcapasso Definitivo (MPD)"},
+            {id: 1,label: "Cardiodesfibrilador Implantável (CDI)"},
+            {id: 2,label: "Tratamento por Ressincronização Cardíaca (TRC)"},
+        ]
+    }
 }
 
 module.exports = Interventions
