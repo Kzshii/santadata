0e463c382273ae8df181145a8187e3341dc32d26
JoséGuilherme <guislima47@gmail.com>
2018-03-29 21:26:34 -0300

    [ Issue #6 ] - create Local-Storage Lib

--- front/src/lib/localStorage.js
+++ front/src/lib/localStorage.js
@@ -1,12 +1,28 @@
 
 function LocalStorage() {}
 
-LocalStorage.save = (storageKey,jsonKey,data) => {
-  let storage = JSON.parse(localStorage.getItem(storageKey));
-  storage[jsonKey]= data;
-  localStorage.setItem(storageKey, JSON.stringify(storage));
-  console.log("LocalStorageLib")
-  console.log(localStorage);
+LocalStorage.save = (data,key, ...args) => {
+  console.log("LocalStorage.Save:")
+  const storage = JSON.parse(localStorage.getItem(key));
+  let pointer = storage;
+  let keys = Object.keys(storage);
+  let name;
+  const lastArg = args[args.length - 1];
+
+  for (let i = 0 ; i < args.length - 1 ; i++) {
+    keys = Object.keys(pointer);
+    name= args[i];
+    if (!(name in keys)){
+      pointer[name]= {};
+      pointer= pointer[name];
+    }else{
+      pointer = pointer[name];
+    }
+  }
+  pointer[lastArg] = data;
+  localStorage.setItem(key,JSON.stringify(storage));
 }
+
+
 
 export default LocalStorage;
