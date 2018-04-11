1eeabbe9ce3784a16ed74ae95fdec46a97d4c423
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-04-10 22:42:49 -0300

    Refactoring: Prepare

--- rest/controller/generics/prepare.js
+++ rest/controller/generics/prepare.js
@@ -20,87 +20,53 @@ var Exams = require("../../model/physical_exam.js")
 var Base = require( '../generic.js' );
 var Prepare = Object.create(Base);
 
+//TODO: Refatorar para ficar generico e buscar do Mongo
+Prepare.types = {
+	cardio:{
+		anamnese: Anamnese.types,
+		evidences: Evidences.types,
+		interventions: Interventions.types,
+		medicines: Medicines.types,
+		predictors: Predictors.type,
+		prevention: Prevention.types,
+		exams: Exams.types
+	}
+}
+
+
 /*
 * Funções genéricas
 */
 
-Prepare.response = function (res,data){
+Prepare.response = (res,data)=>{
 	res.send(JSON.stringify(Prepare.format_response(data)));
 }
 
-
-Prepare.session = function(req,res){
+/*
+* Get Session data
+* @Get /{ambulatory}/{session}/
+**/
+Prepare.session = (req,res)=>{
 	Prepare.get_prepare_data(req);
-	type = {}
-
-	switch(Prepare.url_data_prepare.session){
-		case "anamnese":
-			type = Anamnese.types
-		break;
-
-		case "evidences":
-			type = Evidences.type
-		break;
-
-		case "interventions":
-			type = Interventions.types
-		break;
-
-		case "medicines":
-			type = Medicines.types
-		break;
-
-		case "predictors":
-			type = Predictors.types
-		break;
-
-		case "prevention":
-			type = Prevention.types
-		break;
-
-		case "exams":
-			type = Exams.types
-		break;
-	}
+	let type = Prepare.types[Prepare.url_data_prepare.ambulatory][Prepare.url_data_prepare.session]
 
 	Prepare.response(res,type)
 }
 
-Prepare.ambulatory = function(req,res){
-	Prepare.get_prepare_data(req);
-
-}
-
 /*
-* Funcoes de preparacao de formularios
-*/
+* Get Ambulatory data
+**/
+Prepare.ambulatory = (req,res)=>{
+	Prepare.get_prepare_data(req);
+	let ambulatoryes = Object.getKeys(Prepare.types)
+	let result = {}
+	ambulatoryes.forEach(
+		(amb)=>{
+			result[amb] = Object.getKeys(Prepare.types[amb])
+		}
+	)
 
-Prepare.anamnese = function(req,res){
-	Prepare.response(res,Anamnese.types)
-}
-
-Prepare.evidences = function(req,res){
-	Prepare.response(res,Evidences.types)
-}
-
-Prepare.interventions = function(req,res){
-	Prepare.response(res,Interventions.types)
-}
-
-Prepare.medicines = function(req,res){
-	Prepare.response(res,Medicines.types)
-}
-
-Prepare.predictors = function(req,res){
-	Prepare.response(res,Predictors.types)
-}
-
-Prepare.prevention = function(req,res){
-	Prepare.response(res,Prevention.types)
-}
-
-Prepare.exams = function(req,res){
-	Prepare.response(res,Exams.types)
+	Prepare.response(res,result)
 }
 
 module.exports = Prepare;
